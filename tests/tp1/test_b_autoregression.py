540350f1fec8ed446a643530767c80a5987a7ad1
Valentin Iovene <val@too.gy>
2024-09-12 23:30:38 +0200

    fix non-stationarity test

--- tests/tp1/test_b_autoregression.py
+++ tests/tp1/test_b_autoregression.py
@@ -1,7 +1,8 @@
 import numpy as np
-import pytest
 import numpy.testing
-from dlstp.tp1.b_autoregression import AutoregressiveModel
+import pytest
+
+from dlstp.tp1.b_autoregression import AutoregressiveModel, NonStationaryError
 
 
 def test_init():
@@ -10,21 +11,20 @@ def test_init():
 
 
 def test_forecast():
-  ar = AutoregressiveModel(weights=np.array([1, 0.4, 0.9]))
+  ar = AutoregressiveModel(weights=np.array([1, 0.25, 0.7]))
   generated = ar.forecast(
     past_observations=np.array([1, 0.5]),
     n_samples=3,
   )
-  expected = np.array([1, 0.5, 1.85, 2.865, 4.3185])
+  expected = np.array([1, 0.5, 1.6, 2.245, 2.9715])
   numpy.testing.assert_allclose(generated, expected)
 
 
 @pytest.mark.parametrize(
   "weights",
   [
-    np.array([0, -0.9]),
     np.array([0, 0.9]),
-    np.array([0.1, 0.4, -0.09]),
+    np.array([0.1, 0.4, 0.09]),
     np.array([1, -0.02, 0.2, -0.03]),
     np.array([-0.04, -0.84, 0.0, -0.025]),
   ],
@@ -39,3 +39,17 @@ def test_fit(weights):
   fitted_weights = AutoregressiveModel().fit(data, order=order).weights
   assert fitted_weights is not None
   numpy.testing.assert_allclose(fitted_weights, weights, atol=0.01)
+
+
+@pytest.mark.parametrize(
+  "weights",
+  [
+    np.array([0, -0.3]),
+    np.array([0, 1.3]),
+    np.array([0.1, 0.4, -0.09]),
+    np.array([11, 0.4, 0.9]),
+  ],
+)
+def test_non_stationary(weights):
+  with pytest.raises(NonStationaryError):
+    AutoregressiveModel(weights)
