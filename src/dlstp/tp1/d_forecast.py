57fc9a38fb2732f864e8aaf32b8fd772ee81ff0d
Paul Hartmann <paul.hartmann@epita.fr>
2024-10-02 16:10:43 +0200

    feat(add): add last

--- src/dlstp/tp1/d_forecast.py
+++ src/dlstp/tp1/d_forecast.py
@@ -3,8 +3,27 @@ import pandas as pd
 
 from dlstp.tp1.b_autoregression import AutoregressiveModel
 
+def convert_pct_change_to_price(pct_change: list, initial_price: float) -> list:
+  price = [initial_price]
+  for change in pct_change:
+    next_price = price[-1] * (1 + change)
+    price.append(next_price)
+  return price[1:]
+
+
+def predict_monthly_ari(month_prices: pd.Series, prev_month: pd.Series) -> pd.Series:
+  length_month_prices = len(month_prices)
+  statonary_prices = month_prices.pct_change().dropna()
+  model = AutoregressiveModel()
+  train_model = model.fit(data=statonary_prices.to_numpy(), order=1)
+  prediction = train_model.forecast(past_observations=statonary_prices.to_numpy(), n_samples=length_month_prices)
+  prediction = prediction[-length_month_prices:]
+  prediction = convert_pct_change_to_price(prediction.tolist(), prev_month.iloc[-1])
+  t = pd.Series(data=prediction, index=month_prices.index)
+  return t
 
 def fit_predict_ar1(prices: pd.Series) -> pd.Series:
+
   """
   You are a young student who thinks he's gonna make some good money on the market
   against the big guys from Wall Street. Your last idea is to use an autoregressive
@@ -32,4 +51,34 @@ def fit_predict_ar1(prices: pd.Series) -> pd.Series:
     - Careful about future information leakage (in the input of the model)
 
   """
-  raise NotImplementedError()
+
+  prev_month = pd.Series()
+  forecasted_prices = pd.Series()
+  monthly_series = prices.groupby([pd.Grouper(freq='MS')])
+  for month, month_prices in monthly_series:
+    if prev_month.empty:
+      prev_month = month_prices
+      continue
+    forecasted_monthly = predict_monthly_ari(month_prices, prev_month)
+    if forecasted_prices.empty:
+      forecasted_prices = forecasted_monthly
+    else:
+      forecasted_prices = pd.concat([forecasted_prices, forecasted_monthly])
+    prev_month = month_prices
+  return forecasted_prices
+
+
+
+
+  
+
+# Create a Series with daily data for four months
+data = range(1, 122)  # Values from 1 to 121 (31 days in January + 29 in February + 31 in March + 30 in April)
+index = pd.date_range(start='2020-01-01', periods=121, freq='D')  # Daily frequency for 4 months
+
+# Create the Series
+four_month_series = pd.Series(data=data, index=index)
+
+t = fit_predict_ar1(four_month_series)
+
+
